1a099b57d4c8512ea571b9df907d5ee644afdb59
Shawn <singlex@foxmail.com>
2022-05-28 14:00:27 +0800

    make a language xaml generator.

--- glossary_maker.py
+++ glossary_maker.py
@@ -4,6 +4,21 @@ import copy
 from googletrans import Translator
 from httpcore import SyncHTTPProxy
 
+Special_Characters_in_XAML_Keys = ['&', '<', '>', '\r', '\n']
+Special_Characters_in_XAML_Values = ['&amp;', '&lt;', '&gt;', '&#13;', '&#10;']
+
+
+def XamlReturnToNormalReturn(string: str) -> str:
+    for i in range(len(Special_Characters_in_XAML_Keys)):
+        string = string.replace(Special_Characters_in_XAML_Values[i], Special_Characters_in_XAML_Keys[i])
+    return string
+
+
+def NormalReturnToXamlReturn(string: str) -> str:
+    for i in range(len(Special_Characters_in_XAML_Keys)):
+        string = string.replace(Special_Characters_in_XAML_Keys[i], Special_Characters_in_XAML_Values[i])
+    return string
+
 
 class glossary:
     def __init__(self):
@@ -15,23 +30,15 @@ class glossary:
         self.ROW_LANG_CODE_GOOGLE = 2
         self.ROW_LANG_NAME = 3
 
-    @staticmethod
-    def XamlReturnToNormalReturn(string: str) -> str:
-        return string.replace('&#13;&#10;', '\r\n')
-
-    @staticmethod
-    def NormalReturnToXamlReturn(string: str) -> str:
-        return string.replace('\r\n', '&#13;&#10;')
-
     def load_csv(self, csv_file_name: str, encoding: str = 'utf-8'):
-        lines = []
+        lines = []  # [[key, english_word, a, b, c, d], [key1, w1, ...], ...]
         with open(csv_file_name, encoding=encoding)as f:
             reader = csv.reader(f)
             for row in reader:
                 lines.append(row)
         key_column_index = 0
         en_column_index = 1
-        self.keys = [line[key_column_index] for line in lines]
+        self.keys = [line[key_column_index].replace('"', '') for line in lines]
         self.english_words = [line[en_column_index] for line in lines]
         for col in range(len(lines[0])):
             if col == key_column_index or col == en_column_index:
@@ -54,8 +61,8 @@ class glossary:
                     continue
                 # 没有内容时才进行翻译
                 if column[row] == '':
-                    txt = translator.translate(self.XamlReturnToNormalReturn(self.english_words[row]), dest=column[self.ROW_LANG_CODE_GOOGLE]).text
-                    column[row] = self.NormalReturnToXamlReturn(txt)
+                    txt = translator.translate(XamlReturnToNormalReturn(self.english_words[row]), dest=column[self.ROW_LANG_CODE_GOOGLE]).text
+                    column[row] = NormalReturnToXamlReturn(txt)
                 else:
                     column[row] = ''
 
@@ -72,6 +79,20 @@ class glossary:
             for row in range(len(column)):
                 if row > self.ROW_LANG_NAME:
                     column[row] = ""
+
+    def save_to_xaml(self, encoding: str = 'utf-8'):
+        langs = [self.english_words, *self.columns]
+        for lang in langs:
+            code = lang[self.ROW_LANG_CODE_ISO]
+            name = lang[self.ROW_LANG_NAME]
+            xaml_file_name = code + '.xaml'
+            with open(xaml_file_name, 'w', encoding=encoding, newline='') as f:
+                f.write('<ResourceDictionary xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" xmlns:s="clr-namespace:System;assembly=mscorlib" xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml">\r\n')
+                f.write('    <s:String x:Key="language_name">' + name + '</s:String>\r\n')
+                for row in range(len(self.keys)):
+                    f.write('    <s:String x:Key="' + self.keys[row] + '">' + lang[row] + '</s:String>\r\n')
+                f.write('</ResourceDictionary>')
+            pass
 
     def clone(self):
         return copy.deepcopy(self)
@@ -93,6 +114,9 @@ if __name__ == '__main__':
     # translate
     t = g.clone()
     t.do_translate(translator)
-    t.save_csv(CSV_FILE_NAME + '_translated_part.csv')
+    t.save_csv(CSV_FILE_NAME + '_translated_by_google.csv')
     g.merge_columns(t)
     g.save_csv(CSV_FILE_NAME + '_translated_full.csv')
+    g.save_to_xaml()
+
+    # save xaml
